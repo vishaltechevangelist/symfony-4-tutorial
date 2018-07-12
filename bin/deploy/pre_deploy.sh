39194b70d8d0dec72758a703c01dd779b5cafefe
Vishal Saxena <vishal.saxena@naukri.com>
2018-07-12 09:55:59 +0530

    adding force full argument in removing directory

--- bin/deploy/pre_deploy.sh
+++ bin/deploy/pre_deploy.sh
@@ -1,7 +1,7 @@
 # Remove symlink
-sudo rm -R /var/www/micropost_old && \
-sudo cp -R /var/www/micropost_current /var/www/micropost_old/ && \
+sudo rm -Rf /var/www/micropost_old && \
+sudo cp -Rf /var/www/micropost_current /var/www/micropost_old/ && \
 sudo rm /var/www/micropost && \
-sudo rm -R /var/www/micropost_current && \
+sudo rm -Rf /var/www/micropost_current && \
 # Create symlink to older version && \
 sudo ln -s /var/www/micropost_old /var/www/micropost
